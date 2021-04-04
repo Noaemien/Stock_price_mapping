bdd389a9fc629af1f4c87d4c8befb4b1b4cf0d42
Noaemien <noaemien@gmail.com>
2021-04-05 00:01:43 +0200

    added stop button and live cost display.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import numpy as np
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, json
 from nn_module import Neural_Network
-
 app = Flask(__name__)
 
 
@@ -13,14 +12,16 @@ X = np.random.uniform(-1, 5, (1, 500))
 Y = X ** 2
 
 nn = Neural_Network(X, Y, [128, 128, 1], optimisation_function="GRADIENTDESCENT")
-
+cost = 0
 #Gets called from main.js after a click of the "run" button
 @app.route("/nn_iteration/<float:alpha>")
 def nn_iteration(alpha):
+    global cost
     #print("Success !")
     #print(alpha)
-    cost = nn.train(alpha, 1)
-    return jsonify(cost)
+    if alpha != 0:
+        cost = nn.train(alpha, 1)
+    return json.dumps(cost)
 
 
 #Resets weights and biases when called from main.js
--- static/js/main.js
+++ static/js/main.js
@@ -1,28 +1,51 @@
-
+var stop = false;
 //triggers the function linked to "/nn_iteration/<float:alpha>" when button "run_button" is pressed.
 $(function() {
     $('#run_button').on('click', function(e) {
       var link = "/nn_iteration/" + document.getElementById("alpha_input").value.toString();
       var iterations = document.getElementById("iterations").value; 
       var i = 0;
+      stop = false;
       e.preventDefault();
       function nn_iteration(){
-        if (i < iterations){
+        if (i < iterations && stop == false){
           $.getJSON(link,          //run nn_iteration function in app.py
               function(data) {
                 i++;
                 nn_iteration();    //Loop structured as a callback so that the neural network iteration in the app.py file has the time to finish.
+                var cost = $.parseJSON(data); //retrieve json sent by the nn_iteration function in app.py
+                document.getElementById("cost").innerHTML = cost.toString();
           });
+        } else if(stop == true){
+          stop == false;
         }
+      }      
+      function get_cost(a){
+        $.get("/nn_iteration/0", function(data){
+          var cost = $.parseJSON(data);
+          console.log(cost);
+        });
       }
       nn_iteration();
+
       return false;
     });
   });
 
+//turn stop to true on click of "stop_button"
+$(function() {
+  $('#stop_button').on('click', function(e) {
+    stop = true;
+    e.preventDefault();
+    return false;
+  });
+});
+
+
 //triggers the function linked to "/init_params" url when button "init_params" is pressed.
 $(function() {
   $('#init_params').on('click', function(e) {
+    stop = true;
     var link = "/init_params";
     e.preventDefault();
     $.getJSON(link,
